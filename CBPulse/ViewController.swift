858ad84e779fbbc7ffee252e3e32c5e449adcbaf
Bill Cheng <so89898@gmail.com>
2016-06-04 02:35:19 +0800

    Initial Commit

---
+++ CBPulse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CBPulse
+//
+//  Created by Bill Cheng on 16/6/4.
+//  Copyright © 2016年 R3 Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
